e309fef32d4ba61be2e28bb5929e4b3ca0b258d5
Doctor_Igroman_LoL <dr.igroman.lol@gmail.com>
2019-02-05 14:12:11 +0900

    Решение 1 задачи (хард)

--- Hard/Lesson_4_Task.py
+++ Hard/Lesson_4_Task.py
@@ -6,6 +6,7 @@
 #
 # Ваша задача исправить ошибки логики, и выполнить проверки данных, которые вводит пользователь.
 # Обязательно убедитесь, что вы выполнили все проверки, попробуйте сами сломать свою программу вводя неверные данные!
+import re
 
 person1 = {'card': 4276123465440000, 'pin': 9090, 'money': 100.90}
 person2 = {'card': 4276123465440001, 'pin': 9091, 'money': 200.90}
@@ -13,6 +14,9 @@ person3 = {'card': 4276123465440002, 'pin': 9092, 'money': 300.90}
 
 bank = [person1, person2, person3]
 
+pattern_login = '^[0-9]{16}[ ][0-9]{4}$'
+pattern_point = '^[0-9]$'
+pattern_money = '(^[0-9]+[\.][0-9]{0,2}$|^[0-9]+$)'
 
 def get_person_by_card(card_number):
     for person in bank:
@@ -31,7 +35,7 @@ def check_account(person):
 
 
 def withdraw_money(person, money):
-    if person['money'] - money == 0:
+    if person['money'] - money >= 0:    # ==
         person['money'] -= money
         return 'Вы сняли {} рублей.'.format(money)
     else:
@@ -39,32 +43,47 @@ def withdraw_money(person, money):
 
 
 def process_user_choice(choice, person):
-    if choice == '1':
+    if choice == 1:
         print(check_account(person))
-    elif choice == '2':
-        count = float(input('Сумма к снятию:'))
-        print(withdraw_money(person, count))
+    elif choice == 2:
+        count = input('Сумма к снятию: ')
+        if re.search(pattern_money, str(count)):
+            count = float(count)
+            print(withdraw_money(person, count))
+        else:
+            print('Некорректно введен сумма.')
+    else:
+        print('Некорректно введен пункт.')
+
 
 
 def start():
-    card_number, pin_code = input('Введите номер карты и пин код через пробел:').split()
 
-    card_number = int(card_number)
-    pin_code = int(pin_code)
-    person = get_person_by_card(card_number)
-    if person and is_pin_valid(person, pin_code):
-        while True:
-            choice = int(input('Выберите пункт:\n'
-                               '1. Проверить баланс\n'
-                               '2. Снять деньги\n'
-                               '3. Выход\n'
-                               '---------------------\n'
-                               'Ваш выбор:'))
-            if choice == 3:
-                break
-            process_user_choice(choice, person)
+    valid_login = input('Введите номер карты и пин код через пробел: ')
+    if re.search(pattern_login, valid_login):
+        card_number, pin_code = valid_login.split()
+        card_number = int(card_number)
+        pin_code = int(pin_code)
+        person = get_person_by_card(card_number)
+        if person and is_pin_valid(person, pin_code):
+            while True:
+                choice = input('Выберите пункт: \n'
+                                   '1. Проверить баланс\n'
+                                   '2. Снять деньги\n'
+                                   '3. Выход\n'
+                                   '---------------------\n'
+                                   'Ваш выбор: ')
+                if re.search(pattern_point, choice):
+                    choice = int(choice)
+                    if choice == 3:
+                        break
+                    process_user_choice(choice, person)
+                else:
+                    print('Безобразничаем (~_~)')
+            else:
+                print('Номер карты или пин код введены не верно!')
     else:
-        print('Номер карты или пин код введены не верно!')
+        print('Еще раз не правильно введети, сожру вашу карту Σ(▼□▼メ)')
 
 
 start()
